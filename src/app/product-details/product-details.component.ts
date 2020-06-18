7026d148108a31d3258884289404626f0f78a2fb
AnnKemp <ann.kemp@scarlet.be>
2020-06-18 20:59:54 +0200

    making de component for the cart view after making the service

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router"; // is specific to each routed component that Angular Router loads. It contains information about the route, its parameters, and additional data associated with the route
 
 import { products} from "../products";
+import { CartService } from "../cart.service";
 
 @Component({
   selector: 'app-product-details',
@@ -10,14 +11,17 @@ import { products} from "../products";
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-  product:
 
-  constructor(private route: ActivatedRoute, ) { // by injecting the ActivatedRoute, you are configuring the component to use a service.
+  constructor(private route: ActivatedRoute, private cartService: CartService ) { // by injecting the ActivatedRoute, you are configuring the component to use a service. The same for CartService.
+  }
 
-}
-
+  addToCart(product){
+    this.cartService.addToCart(product);
+    window.alert('Your product has been added to the cart!');
+  }
+  product;
   ngOnInit() {
-    this.route.paramMap.subscribe(params => {
+    this.route.paramMap.subscribe(params => { // om de parameters uit de route te halen en met subscribe een soort constant contact te houden mochten er iets veranderen/gebeuren . . .
       this.product = products[+params.get('productId')];
     });
   }
