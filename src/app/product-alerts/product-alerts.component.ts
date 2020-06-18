6113c8665af28e58ebf7f364cca9ac8cdeadafce
AnnKemp <ann.kemp@scarlet.be>
2020-06-18 12:04:02 +0200

    adding input and outpunt decorators

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Input } from "@angular/core"; // da's voor de decorator @input te gebruiken
+import { Component, OnInit } from '@angular/core';// OnInit is een interface
+import { Input } from "@angular/core"; // da's om de decorator @input te gebruiken
+import { Output, EventEmitter } from "@angular/core";
 
 @Component({ // dit is de decorator, it provides metadata about the component
   selector: 'app-product-alerts',
@@ -8,9 +9,5 @@ import { Input } from "@angular/core"; // da's voor de decorator @input te gebru
 })
 export class ProductAlertsComponent implements OnInit {
   @Input() product; // this is a property named product with an @Input decorator. The @Input decorator indicates that the property value passes in from the component's parent, the product list component
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  @Output() notify= new EventEmitter(); //property notify, this allows the product alert component to emit an event when the value of the notify property changes, the output decorator stuurt de data terug naar boven in de three naar product-list.components.ts
 }
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -2,15 +2,19 @@ import { Component } from "@angular/core";
 
 import { products } from '../products';
 
-@Component({
-  selector: 'app-product-list',
+@Component({ // dit is de decorator
+  selector: 'app-product-list', // the selector identifies the component. The selector is the name you give the Angular component when it is rendered as an HTML element on the page. By convention, Angular component selectors begin with the prefix app-, followed by the component name.s
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent{
+export class ProductListComponent{ // dit the component definition
   // dit is dus een products property (normaal wordt dan toch een waarde toegewezen na dubbel punt?
   products = products;
   share(){
     window.alert('The product has been shared!');
   }
+  onNotify(){
+    window.alert('You will be notified when the product goes on sale');
+  }
 }
+
