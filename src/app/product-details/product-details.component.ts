022c81c421b30ef51aa3082bc4a75e96d9ba8f28
AnnKemp <ann.kemp@scarlet.be>
2020-06-18 12:29:40 +0200

    adding routing to go to productdetails

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,16 @@
+// generated product-details component via the terminal with: ng g component name
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
