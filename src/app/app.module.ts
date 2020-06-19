4b70aa5aaa6bed94ee8928e985f40bec4066345a
AnnKemp <ann.kemp@scarlet.be>
2020-06-19 14:21:09 +0200

    added shipping-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
+// This file contains imports and functionality that is available to the entire app
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule} from "@angular/forms";
 import {RouterModule} from "@angular/router";
+import { HttpClientModule} from "@angular/common/http";// Angular's HttpClientModule registers the providers your app needs to use a single instance of the HttpClient service throughout your app
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from "./top-bar/top-bar.component";
@@ -9,6 +11,7 @@ import { ProductListComponent } from "./product-list/product-list.component";
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   declarations: [
@@ -17,10 +20,12 @@ import { CartComponent } from './cart/cart.component';
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
-    CartComponent
+    CartComponent,
+    ShippingComponent
   ],
-  imports: [
+  imports: [ // add the HttpClientModule to @ngModule() imports array to register Angular's HttpClient providers globally
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from "@angular/common/http";// Now that the appModule imports the HttpClientModule, the next step is to inject the HttpClient service into your service so your app can fetch data and interact with external APIs and resources.
 
-@Injectable({
+@Injectable({ // services maken gebruik van @Injectables
   providedIn: 'root'
 })
 export class CartService {
   items = []; // items is a property and used to store the array of the current products in the cart
+  constructor(private http: HttpClient) { // inject the HttpClient to use
+  }
 
   addToCart(product){
     this.items.push(product);
@@ -12,8 +15,11 @@ export class CartService {
   getItems(){
     return this.items;
   }
-  clearCart(){
+  clearCart(){ // functies hoeven in Angular het woordje 'function' er niet voor
     this.items = [];
     return this.items;
+  }
+  getShippingPrices(){
+    return this.http.get('/assets/shipping.json');
   }
 }
