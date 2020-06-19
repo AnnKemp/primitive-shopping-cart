9e95237153bd494b2cc5d237b90f47696e2acb66
AnnKemp <ann.kemp@scarlet.be>
2020-06-19 11:24:02 +0200

    cart works!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,7 @@ import { CartComponent } from './cart/cart.component';
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent}, // het pad naar productdetails om de details/extra info van producten te bekijken, doorverbinden via productId.
+      { path: 'cart', component: CartComponent },
     ])
   ],
   providers: [],
