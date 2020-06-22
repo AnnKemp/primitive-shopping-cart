c4d7378ee1a7202d5c239828a804e2abe7b0cbff
AnnKemp <ann.kemp@scarlet.be>
2020-06-22 14:14:03 +0200

    trying to add picture

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,16 +1,19 @@
 export const products = [
   {
     name: 'Phone XL',
+    photo: 'phone_3.png',
     price: 799,
     description: 'A large phone with one of the best screens'
   },
   {
     name: 'Phone Mini',
+    photo: 'phone_2.png',
     price: 699,
     description: 'A great phone with one of the best cameras'
   },
   {
     name: 'Phone Standaard',
+    photo: 'phone_1.png',
     price: 299,
     description: ''
   }
