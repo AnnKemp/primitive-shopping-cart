3937b46a265213b1c0b4bf3f1ee36b3802b7de4d
AnnKemp <ann.kemp@scarlet.be>
2020-06-23 14:46:04 +0200

    changing the way the add the map, just changed package.json, added a dependency

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+// key for CHVOxBhwGMvtRCuGizEp for the here.com map
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    let L;
+    let mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: 'pk.eyJ1IjoiYW5ua2VtcCIsImEiOiJja2JycDg0MDMyNjJpMndwOXRlYWd3enA3In0.UIG13B0o0gHaSQh-a_9iTg'
+    }).addTo(mymap);
+  }
 }
